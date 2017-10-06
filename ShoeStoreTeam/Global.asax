4e5c65708d4e7583b8e04e6f24e4404248ef6e85
Trieu Ngo <vantrieupy96@outlook.com>
2017-10-06 10:05:35 +0700

    Add project files.

---
+++ ShoeStoreTeam/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShoeStoreTeam.MvcApplication" Language="C#" %>
---
+++ ShoeStoreTeam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoeStoreTeam.Startup))]
+namespace ShoeStoreTeam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
